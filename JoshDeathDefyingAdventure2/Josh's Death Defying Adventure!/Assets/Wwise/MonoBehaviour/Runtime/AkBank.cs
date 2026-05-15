6669d973211909bdb765942a399d5ff063c90d94
loweryjoshua <52studiosCGI@gmail.com>
2026-05-14 22:31:35 -0500

    Upgraded Wwise Integration to 2025

--- JoshDeathDefyingAdventure2/Josh's Death Defying Adventure!/Assets/Wwise/MonoBehaviour/Runtime/AkBank.cs
+++ JoshDeathDefyingAdventure2/Josh's Death Defying Adventure!/Assets/Wwise/MonoBehaviour/Runtime/AkBank.cs
@@ -1,4 +1,4 @@
-#if ! (UNITY_DASHBOARD_WIDGET || UNITY_WEBPLAYER || UNITY_WII || UNITY_WIIU || UNITY_NACL || UNITY_FLASH || UNITY_BLACKBERRY) // Disable under unsupported platforms.
+#if !(UNITY_QNX) // Disable under unsupported platforms.
 /*******************************************************************************
 The content of this file includes portions of the proprietary AUDIOKINETIC Wwise
 Technology released in source code form as part of the game integration package.
@@ -13,7 +13,7 @@ Licensees holding valid licenses to the AUDIOKINETIC Wwise Technology may use
 this file in accordance with the end user license agreement provided with the
 software or, alternatively, in accordance with the terms contained
 in a written agreement between you and Audiokinetic Inc.
-Copyright (c) 2024 Audiokinetic Inc.
+Copyright (c) 2026 Audiokinetic Inc.
 *******************************************************************************/
 
 [UnityEngine.AddComponentMenu("Wwise/AkBank")]
@@ -45,6 +45,8 @@ public class AkBank : AkTriggerHandler
 	protected override void Awake()
 	{
 #if UNITY_EDITOR
+		AkUnitySoundEngineInitialization.Instance.initializationDelegate += HandleEvent;
+
 		if (UnityEditor.BuildPipeline.isBuildingPlayer || AkUtilities.IsMigrating)
 		{
 			return;
@@ -57,7 +59,6 @@ public class AkBank : AkTriggerHandler
 			data.ObjectReference = reference;
 			AkWwiseTypes.DragAndDropObjectReference = null;
 		}
-		AkUnitySoundEngineInitialization.Instance.initializationDelegate += HandleEvent;
 #endif
 
 		base.Awake();
@@ -130,11 +131,12 @@ public class AkBank : AkTriggerHandler
 	protected override void OnDestroy()
 	{
 #if UNITY_EDITOR
+		AkUnitySoundEngineInitialization.Instance.initializationDelegate -= HandleEvent;
+
 		if (UnityEditor.BuildPipeline.isBuildingPlayer || AkUtilities.IsMigrating)
 		{
 			return;
 		}
-		AkUnitySoundEngineInitialization.Instance.initializationDelegate -= HandleEvent;
 #endif
 
 		base.OnDestroy();
@@ -184,4 +186,4 @@ public class AkBank : AkTriggerHandler
 #endif
 	#endregion
 }
-#endif // #if ! (UNITY_DASHBOARD_WIDGET || UNITY_WEBPLAYER || UNITY_WII || UNITY_WIIU || UNITY_NACL || UNITY_FLASH || UNITY_BLACKBERRY) // Disable under unsupported platforms.
+#endif // #if !(UNITY_QNX) // Disable under unsupported platforms.
--- JoshDeathDefyingAdventure2/Josh's Death Defying Adventure!/Assets/Wwise/MonoBehaviour/Runtime/AkEvent.cs
+++ JoshDeathDefyingAdventure2/Josh's Death Defying Adventure!/Assets/Wwise/MonoBehaviour/Runtime/AkEvent.cs
@@ -1,4 +1,4 @@
-#if ! (UNITY_DASHBOARD_WIDGET || UNITY_WEBPLAYER || UNITY_WII || UNITY_WIIU || UNITY_NACL || UNITY_FLASH || UNITY_BLACKBERRY) // Disable under unsupported platforms.
+#if !(UNITY_QNX) // Disable under unsupported platforms.
 /*******************************************************************************
 The content of this file includes portions of the proprietary AUDIOKINETIC Wwise
 Technology released in source code form as part of the game integration package.
@@ -13,8 +13,10 @@ Licensees holding valid licenses to the AUDIOKINETIC Wwise Technology may use
 this file in accordance with the end user license agreement provided with the
 software or, alternatively, in accordance with the terms contained
 in a written agreement between you and Audiokinetic Inc.
-Copyright (c) 2024 Audiokinetic Inc.
+Copyright (c) 2026 Audiokinetic Inc.
 *******************************************************************************/
+
+using AK.Wwise.Unity.Logging;
 
 /// <summary>
 ///     Event callback information.
@@ -164,10 +166,11 @@ public class AkEvent : AkDragDropTriggerHandler
 
 		data.Post(gameObj);
 	}
-	
+
 	protected void OnDisable()
 	{
-		if (stopSoundOnDestroy)
+		var akGameObj = gameObject.GetComponent<AkGameObj>();
+		if (stopSoundOnDestroy && akGameObj != null && akGameObj.GameObjIsRegistered())
 		{
 			var gameObj = useOtherObject && otherGameObject != null ? otherGameObject : gameObject;
 			data.ExecuteAction(gameObj, AkActionOnEventType.AkActionOnEventType_Stop, (int)transitionDuration * 1000, curveInterpolation);
@@ -242,7 +245,7 @@ public class AkEvent : AkDragDropTriggerHandler
 		var count = oldCallbackData.callbackFlags.Count;
 		if (count != oldCallbackData.callbackFunc.Count || count != oldCallbackData.callbackGameObj.Count)
 		{
-			UnityEngine.Debug.LogWarning("WwiseUnity: Inconsistent callback data!");
+			WwiseLogger.Warning("Inconsistent callback data!");
 			return hasMigrated;
 		}
 
@@ -256,7 +259,7 @@ public class AkEvent : AkDragDropTriggerHandler
 			data.FindPropertyRelative("GameObject").objectReferenceValue = oldCallbackData.callbackGameObj[i];
 			data.FindPropertyRelative("FunctionName").stringValue = oldCallbackData.callbackFunc[i];
 			data.FindPropertyRelative("Flags.value").intValue = oldCallbackData.callbackFlags[i];
-			UnityEngine.Debug.Log("WwiseUnity: Migrated Callback for function \"" + oldCallbackData.callbackFunc[i] + "\" on <" + oldCallbackData.callbackGameObj[i] + "> with flags <" + (AkCallbackType)oldCallbackData.callbackFlags[i] + ">.");
+			WwiseLogger.Log("Migrated Callback for function \"" + oldCallbackData.callbackFunc[i] + "\" on <" + oldCallbackData.callbackGameObj[i] + "> with flags <" + (AkCallbackType)oldCallbackData.callbackFlags[i] + ">.");
 		}
 
 		return true;
@@ -264,4 +267,4 @@ public class AkEvent : AkDragDropTriggerHandler
 #endif
 	#endregion
 }
-#endif // #if ! (UNITY_DASHBOARD_WIDGET || UNITY_WEBPLAYER || UNITY_WII || UNITY_WIIU || UNITY_NACL || UNITY_FLASH || UNITY_BLACKBERRY) // Disable under unsupported platforms.
+#endif // #if !(UNITY_QNX) // Disable under unsupported platforms.
